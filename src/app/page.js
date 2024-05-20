8a1045edb122b143e16fbb2954b85555db800e10
Aditya Gavali <adityagavali0911@gmail.com>
2024-05-20 23:46:52 +0530

    pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="bg-white h-screen">
       <Navbar/>
+      <Footer/>
       
     </main>
   );
